61ce32d0e380bea04f0857310fd2111c28d03bdd
anvi10 <arayabha@purdue.edu>
2018-04-12 01:11:28 -0400

    done with read, write, commit

--- CC.java
+++ CC.java
@@ -54,7 +54,7 @@ public class CC
 
 				}
 
-				//try the write method here
+				//try the write method here and copy this on to t2 and t3
 				if ( t1.get(ptr).charAt(0) == 'W') {
 					//If the current transaction already has a Shared Lock (and no other transaction has any other locks) 
 					//you will upgrade the Shared Lock to an Exclusive lock.
@@ -64,12 +64,17 @@ public class CC
 					String exclusiveLock = "X(" + s + ")" ; 
 
 					if (t1_locks.contains(sharedLock)) {
-						//upgrade the sharedlock to an exclusive lock
+						//upgrade the shared lock to an exclusive lock
+						int index = t1_locks.indexOf(sharedLock);
+						t1_locks.set(index, exclusiveLock);
 					}
 
 					//When a write operation is encountered, attempt to acquire an 
 					//Exclusive Lock on that record. An Exclusive Lock can be acquired 
 					//only if there is no other lock on that record from another transaction. 
+					if ( !t2_locks.contains(sharedLock)  && !t2_locks.contains(exclusiveLock) && !t3_locks.contains(sharedLock) && !t3_locks.contains(exclusiveLock) ) {
+						t1_locks.add(exclusiveLock);
+					}
 
 				}
 
@@ -93,6 +98,30 @@ public class CC
 
 				}
 
+				//try the write method here and copy this on to t2 and t3
+				if ( t2.get(ptr).charAt(0) == 'W') {
+					//If the current transaction already has a Shared Lock (and no other transaction has any other locks) 
+					//you will upgrade the Shared Lock to an Exclusive lock.
+
+					String s = Character.toString( t2.get(ptr).charAt(2) );
+					String sharedLock = "S(" + s + ")" ; 
+					String exclusiveLock = "X(" + s + ")" ; 
+
+					if (t2_locks.contains(sharedLock)) {
+						//upgrade the shared lock to an exclusive lock
+						int index = t2_locks.indexOf(sharedLock);
+						t2_locks.set(index, exclusiveLock);
+					}
+
+					//When a write operation is encountered, attempt to acquire an 
+					//Exclusive Lock on that record. An Exclusive Lock can be acquired 
+					//only if there is no other lock on that record from another transaction. 
+					if ( !t1_locks.contains(sharedLock)  && !t1_locks.contains(exclusiveLock) && !t3_locks.contains(sharedLock) && !t3_locks.contains(exclusiveLock) ) {
+						t2_locks.add(exclusiveLock);
+					}
+
+				}
+
 				if ( t2.get(ptr).charAt(0) == 'C') {
 					t2_locks.clear();
 				}
@@ -109,6 +138,30 @@ public class CC
 					
 					if (  !t1_locks.contains(exclusiveLock) && !t2_locks.contains(exclusiveLock) && !t3_locks.contains(sharedLock) && !t3_locks.contains(exclusiveLock)) {
 						t3_locks.add(sharedLock);
+					}
+
+				}
+
+				//try the write method here and copy this on to t2 and t3
+				if ( t3.get(ptr).charAt(0) == 'W') {
+					//If the current transaction already has a Shared Lock (and no other transaction has any other locks) 
+					//you will upgrade the Shared Lock to an Exclusive lock.
+
+					String s = Character.toString( t1.get(ptr).charAt(2) );
+					String sharedLock = "S(" + s + ")" ; 
+					String exclusiveLock = "X(" + s + ")" ; 
+
+					if (t3_locks.contains(sharedLock)) {
+						//upgrade the shared lock to an exclusive lock
+						int index = t3_locks.indexOf(sharedLock);
+						t3_locks.set(index, exclusiveLock);
+					}
+
+					//When a write operation is encountered, attempt to acquire an 
+					//Exclusive Lock on that record. An Exclusive Lock can be acquired 
+					//only if there is no other lock on that record from another transaction. 
+					if ( !t1_locks.contains(sharedLock)  && !t1_locks.contains(exclusiveLock) && !t2_locks.contains(sharedLock) && !t2_locks.contains(exclusiveLock) ) {
+						t3_locks.add(exclusiveLock);
 					}
 
 				}
